a849b23f7b1bcf60df794686b529b2d9d312c4da
jrogoz <joanna.rogoz33@gmail.com>
2025-12-12 20:41:26 +0100

    core: [#4] Integrate room database - user table

---
+++ app/src/main/java/com/example/blockplanner/UserDao.kt
@@ -0,0 +1,15 @@
+package com.example.blockplanner
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface UserDao {
+
+    @Insert
+    suspend fun insert(user: User)
+
+    @Query("SELECT * FROM users")
+    suspend fun getAllUsers(): List<User>
+}
